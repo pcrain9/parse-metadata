e3d66daef94caa4584ab311d98abcd42aa998693
Perry <80589286+pcrain9@users.noreply.github.com>
2022-02-19 17:21:05 -0600

    app is taking shape, a rough draft table that displays data is working, many small fixes to continue working on.

--- src/App.js
+++ src/App.js
@@ -1,15 +1,26 @@
-import React from "react";
-import { Routes, Route } from "react-router-dom";
+import React, { useState } from "react";
+import { Routes, Route, useNavigate } from "react-router-dom";
 import Typography from "@mui/material/Typography";
 
 import MetaParser from "./components/MetaParser";
+import DataTable from "./components/DataTable";
 
 function App() {
+  const [metaData, setMetaData] = useState([]);
+  const navigate = useNavigate();
+
+  function handleSaveParsedData(data) {
+    setMetaData(data);
+    navigate("/display-results");
+  }
+
   return (
     <div style={{ width:"100%" }}><Typography variant="h3" textAlign="center">Metadata Parser!</Typography>
     <Routes>
-      <Route path="/" element={<MetaParser />} />
-    </Routes></div>
+      <Route path="/" element={<MetaParser onSaveParsedData={handleSaveParsedData} />} />
+      <Route path="/display-results" element={<DataTable data={metaData} />} />
+    </Routes>
+    </div>
   );
 }
 
--- src/components/MetaParser.js
+++ src/components/MetaParser.js
@@ -3,7 +3,7 @@ import Grid from "@mui/material/Grid";
 import TextField from "@mui/material/TextField";
 import Button from "@mui/material/Button";
 
-function MetaParser() {
+function MetaParser(props) {
   const [userInput, setUserInput] = useState("");
   const parser = new DOMParser();
 
@@ -13,14 +13,59 @@ function MetaParser() {
 
   function handleSubmit(event) {
     event.preventDefault();
-    const result = parser
-    .parseFromString(event.target[0].value, "text/html")
-    .getElementsByTagName("meta")
-    
-    for(let i = 0; i<result.length; i++){
-      console.log(typeof result[i].outerHTML)
+
+    //extract all meta tags from pasted document
+    const metaTags = parser
+      .parseFromString(event.target[0].value, "text/html")
+      .getElementsByTagName("meta");
+
+    const linkTags = parser
+      .parseFromString(event.target[0].value, "text/html")
+      .getElementsByTagName("link");
+
+    if (metaTags.length === 0 && linkTags.length === 0) {
+      //error handler
+      return;
     }
+
+    let arr = [];
+    for (let i = 0; i < metaTags.length; i++) {
+      let tmp = { name: "", content: "" };
+      for (let j = 0; j < metaTags[i].attributes.length; j++) {
+        const currentTag = metaTags[i].attributes[j];
+        if (currentTag.name === "property") {
+          tmp.name = currentTag.nodeValue;
+        }
+        if (currentTag.name === "name") {
+          tmp.name = currentTag.nodeValue;
+        }
+        if (currentTag.name === "content") {
+          tmp.content = currentTag.nodeValue;
+        }
+      }
+      if (tmp.content === "") {
+        tmp.content = metaTags[i].nextSibling.data
+      }
+      arr.push(tmp);
+    }
+    
+
+    //if the html has no link tags, pass data up to app
+    if (linkTags.length === 0) {
+      props.onSaveParsedData(arr);
+      return;
+    }
+
+    //if there are link tags, parse them
+    for (let i = 0; i < linkTags.length; i++) {
+      let tmp = { name: "", content: "" };
+      tmp.name = linkTags[i].attributes.rel.textContent;
+      tmp.content = linkTags[i].attributes.href.textContent;
+      arr.push(tmp);
+    }
+    props.onSaveParsedData(arr)
   }
+
   return (
     <Grid
       container
@@ -45,7 +90,7 @@ function MetaParser() {
         <Button type="submit" variant="contained">
           Submit
         </Button>
-      </Grid>
+      </Grid>{" "}
     </Grid>
   );
 }
